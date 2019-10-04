65d718d57f9fe33ad6674cb4e16ec49bc2ecbb23
Przemyslaw Czaus <przemyslaw@czaus.pl>
2019-10-04 16:13:21 +0200

    Lesson 0

---
+++ Lesson0/Lesson0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(5000);
+            Console.WriteLine("Hello World Again!");
+        }
+    }
+}
