802f8a26fd80a75d5e7e0d9238455cea0f78f4d3
zelu96 <47740805+zelu96@users.noreply.github.com>
2019-10-11 15:54:08 +0200

    Create 141360.cs

---
+++ Lesson1/141360.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zad1
+            /*
+            int[] tab1 = { 1, 2, 3, 4, 5, 6 };
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                Console.Write(tab1[i] + ",");
+            }
+            Console.WriteLine("\n");
+            for(int i = 0; i< tab1.Length;i++)
+            {
+                Console.WriteLine(tab1[i]);
+            }
+            Console.WriteLine("\n");
+            for (int i = tab1.Length - 1; i>= 0; i--)
+            {
+                Console.WriteLine(tab1[i]);
+            }
+            Console.ReadKey();
+           */
+
+            // Zad2
+            /*
+            int liczba1 = 3;
+            int liczba2 = 5;
+            int liczba3 = 3;
+            
+            if (liczba1 == liczba2) Console.WriteLine("Liczby sa takie same");
+           
+            else Console.WriteLine("Liczby sa inne");
+            
+            
+            if (liczba1 == liczba3) Console.WriteLine("sa rowne");
+            else Console.WriteLine("nie sa");
+
+            Console.ReadKey();
+            */
+
+
+            
+
+            
+
+
+            
+        }
+        
+    }
+}
