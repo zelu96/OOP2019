9b219174f269b2d5b764a4cbf1a50c68d6c465dc
Przemyslaw Czaus <przemyslaw@czaus.pl>
2019-10-11 15:09:47 +0200

    Additional lesson tasks

---
+++ Lesson1/Lesson1/program102.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class main1
+{
+    public static void Main()
+    {
+        /*1*/
+        int x = 1, y;
+        /*2*/
+        Console.WriteLine(++x);
+        /*3*/
+        Console.WriteLine(x++);
+        /*4*/
+        Console.WriteLine(x);
+        /*5*/
+        y = x++;
+        /*6*/
+        Console.WriteLine(y);
+        /*7*/
+        y = ++x;
+        /*8*/
+        Console.WriteLine(++y);
+        Console.ReadKey();
+    }
+}
