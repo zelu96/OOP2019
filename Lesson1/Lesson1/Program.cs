db8bccc7cafabc3c9f1d28cfc00c51aded6b1de6
Przemyslaw Czaus <przemyslaw@czaus.pl>
2019-10-11 13:55:42 +0200

    Lesson 1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string eva = "variable";
+            string userName;
+            Console.WriteLine("Podaj swoje imie, a nastepnie wcisnij ENTER");
+            userName =  Console.ReadLine();
+            if (userName != "")
+            {
+                Console.WriteLine("Hello " + userName + ".");
+            }
+            else {
+                Console.WriteLine("Nie podałes imienia");
+            }
+            Console.ReadLine();
+        }
+    }
+}
